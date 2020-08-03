efcca4f1af9a88aa3a442895f1494a13152bb9c4
groana <contact@groanamelendez.com>
2020-08-03 23:39:08 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+# I need to split the baguette between everyone
+# equal amount of baguette for each person
+# input = a baguette, number of people
+# baguette length divided by the number of people
+  # result will be the amount each person will get
